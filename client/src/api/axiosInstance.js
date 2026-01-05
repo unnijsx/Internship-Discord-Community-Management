4ea4303f15d515cb538112ede5988f3867f94f2c
unnijsx <unniytman@gmail.com>
2026-01-05 11:58:12 +0530

    vite url

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'https://internship-discord-community-manage.vercel.app/',
+    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000',
 });
 
 export default axiosInstance;
